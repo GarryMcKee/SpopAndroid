56a5c2546aefe612a528f9b9c1ef62ed080504d8
Garry McKee <garry.mckee@hubchat.com>
2017-02-27 20:02:12 +0200

    Update gitignore

--- app/src/main/java/com/example/garrymckee/spop/SpopMainActivity.java
+++ app/src/main/java/com/example/garrymckee/spop/SpopMainActivity.java
@@ -11,10 +11,6 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 public class SpopMainActivity extends AppCompatActivity {
 
     private String spotifyAuthUrl;
