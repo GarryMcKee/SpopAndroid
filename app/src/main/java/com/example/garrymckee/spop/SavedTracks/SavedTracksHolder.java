f43cc732a7b7a67c96515cf3b3fd5c53c0dfc579
GarryMcKee <gmk0232@gmail.com>
2017-05-22 19:27:00 +0100

    Add SavedTracksHolder singleton class

---
+++ app/src/main/java/com/example/garrymckee/spop/SavedTracks/SavedTracksHolder.java
@@ -0,0 +1,34 @@
+package com.example.garrymckee.spop.SavedTracks;
+
+import com.example.garrymckee.spop.Model.Track;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Garry on 22/05/2017.
+ */
+
+public class SavedTracksHolder {
+
+    private static SavedTracksHolder instance;
+
+    private List<Track> savedTracks;
+
+    private SavedTracksHolder() {
+        savedTracks = new ArrayList<>();
+    }
+
+    public SavedTracksHolder getInstance() {
+        if (instance == null) {
+            instance = new SavedTracksHolder();
+            return instance;
+        } else {
+            return instance;
+        }
+    }
+
+    public void setSavedTracks(List<Track> savedTracks) {
+        this.savedTracks = savedTracks;
+    }
+}
